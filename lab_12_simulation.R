fd0ad12b0ea28341cc625a60e80368b375c07b14
gray <shinjoohyung25@gmail.com>
2019-07-31 13:44:00 -0400

    add model_select

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,14 @@ generate_data <- function(n, p) {
   return(list(covariates = covariates, responses = responses))
 }
 
+model_select <- function(covariates, responses, cutoff) {
+  reg_m <- lm(responses ~ covariates)
+  p_values <- summary(reg_m)$coefficients[,"Pr(>|t|)"][-1]
+  index_p <- which(p_values <= cutoff)
+  if (length(index_p) == 0) {
+    return(vector())
+  } else {
+    new_reg <- lm(responses ~ covariates[, index_p])
+    return (summary(new_reg)$coefficients[,"Pr(>|t|)"][-1])
+  }
+}
